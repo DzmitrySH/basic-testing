9e57f62513f3d9669d770c5f1b80c930a72deffe
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-06-30 18:14:34 +0300

    feat: implement mock lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,51 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios', () => {
+  const originalModule = jest.requireActual('axios');
+  return {
+    ...originalModule,
+    create: () => ({
+      get: () => jest.fn((fn) => fn),
+    }),
+  };
+});
+
+const basePost = '/users/1';
+const base = 'https://jsonplaceholder.typicode.com';
+const isData = { id: 1, title: 'User 1' };
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosCreate = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(basePost);
+    expect(axiosCreate).toHaveBeenCalledWith({ baseURL: base });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const instanceOn = axios.create();
+    const axiosCreate = jest.spyOn(instanceOn, 'get');
+    jest.spyOn(axios, 'create').mockReturnValue(instanceOn);
+    await throttledGetDataFromApi(basePost);
+    jest.runOnlyPendingTimers();
+    expect(axiosCreate).toHaveBeenCalledWith(basePost);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const testData = [isData];
+    const axiosCreate = axios.create();
+    jest.spyOn(axiosCreate, 'get').mockResolvedValue({ data: testData });
+    jest.spyOn(axios, 'create').mockReturnValue(axiosCreate);
+    const resultData = await throttledGetDataFromApi(basePost);
+    expect(resultData).toBeTruthy();
+    expect(resultData).toEqual(testData);
   });
 });
